a586506a982840be898d1ae55b5555051879ee44
Felix Barros <felix.barros@gmail.com>
2015-12-23 21:10:04 -0500

    Connect InformationCell to index.html, all stubbed out

---
+++ src/components/InformationCell.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var InformationCell = React.createClass({
+  render: function() {
+    return (<div/>);
+  }
+});
+
+module.exports = InformationCell;
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
 // Main entry point for the application
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+ReactDOM.render(<InformationCell />, document.getElementById('goals'));
